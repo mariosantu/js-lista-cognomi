828b0b89cd3bf5f78148bf7d9f0f821ea4eca6db
Mario Santucci <mario.santucci95@gmail.com>
2021-03-24 13:21:20 +0100

    creazione repo + file + indentazione + array

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Creo array cognomi 
+
+var listaCognomi = [
+    'santucci',
+    'zannettone',
+    'Barletta',
+    'musella',
+    'esposito',
+    'cusano'
+]
+
+console.log(listaCognomi);
+
+//Chiedo il nome all'utente
+
+var cognomeUtente = prompt('Dimmi il tuo cognome :')
+console.log(cognomeUtente);
+
+
+//pusho nomeutente in array
+
+
+//stampo lista ordinata alfabeticamente
+
+//scrivere posizione umana della lista in vui il nuovo utente si trova sia in for che while
+
+
+
+
+
+
+
+
+
+
